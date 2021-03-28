5623f2b5037fd5c9822582adf1c5829b27621917
Suba <sah.suba@gmail.com>
2021-03-28 11:49:03 -0400

    env2.0

--- NR-IES/NR_IES/envs/env2.0_NR_IES_env.py
+++ NR-IES/NR_IES/envs/env2.0_NR_IES_env.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Mar  8 12:04:11 2021
-
 @author: ponki
 """
 import gym
@@ -15,55 +14,46 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 
-    
+
 class NR_IES_v0(gym.Env):
     metadata = {
         'render.modes': ['human'],    
     }
     def __init__(self):
 
-        self.total_t_min = 500000000 
-        self.total_t_max = 700000000
+        self.total_T_min = 500000000 
+        self.total_T_max = 700000000
+        self.total_RE_min = 10000000 
+        self.total_RE_max= 100000000
+        self.e_price_min = 0.10 
+        self.e_price_max = 0.20
 
-        self.total_re_min = 10000000 
-        self.total_re_max= 100000000
-
-        self.e_price_min = 0.00010
-        self.e_price_max = 0.00020
-
-        self.h_price_min = 2
+        self.h_price_min = 1.5
         self.h_price_max = 4
-
         self.bes_min = 2000
         self.bes_max = 100000
-
-        self.tes_min = 1000      
-        self.tes_max = 50000
-
+        self.tes_min = 1000000 
+        self.tes_max = 5000000
         self.hss_min = 10        
-        self.hss_max = 100
-
+        self.hss_max = 50
         self.power_grid_min = 1000        
         self.power_grid_max = 500000000
-
         self.hpr_HTSE_min = 1        
         self.hpr_HTSE_max = 400
-
         self.htr_PEM_min = 1        
         self.htr_PEM_max = 200
-
         self.h_distributed_min = 1        
-        self.h_distributed_max = 500
+        self.h_distributed_max = 600
         
         self.episodlenght = 0
         
         self.low_state = np.array(
-            [self.total_t_min, self.total_re_min, self.e_price_min, self.h_price_min, 
+            [self.total_T_min, self.total_RE_min, self.e_price_min, self.h_price_min, 
              self.bes_min, self.tes_min,self.hss_min, self.power_grid_min,
              self.hpr_HTSE_min,self.htr_PEM_min, self.h_distributed_min], dtype=np.float32)
         
         self.high_state = np.array(
-            [self.total_t_max, self.total_re_max, self.e_price_max, self.h_price_max,
+            [self.total_T_max, self.total_RE_max, self.e_price_max, self.h_price_max,
              self.bes_max, self.tes_max,self.hss_max, self.power_grid_max,
              self.hpr_HTSE_max, self.htr_PEM_max, self.h_distributed_max], dtype=np.float32)
 
@@ -74,12 +64,11 @@ class NR_IES_v0(gym.Env):
         #4 Electric power to battery energy storage (1kwh-100kwh) ?
         #5 Electric power to PEM (5kwh- 50kwh)
         #6 Produced hydrogen to hydrogen energy storage 1-200kg) ?
-
         self.low_action = np.array(
-            [8000, 50000, 1000, 5000, 1 ], dtype=np.float32)
+            [10, 50000, 1000, 5000, 1 ], dtype=np.float32)
         
         self.high_action = np.array(
-            [40000, 800000, 100000, 500000, 200], dtype=np.float32)
+            [50, 800000, 100000, 50000, 200], dtype=np.float32)
         self.action_space = spaces.Box(
             low=self.low_action,
             high=self.high_action,
@@ -92,24 +81,18 @@ class NR_IES_v0(gym.Env):
             shape=(11,),
             dtype=np.float32
         )
-        self.seed()
-
-        # ???
-        self.epen = 0
-        self.tpen = 0
-        self.hpen = 0
+        self.seed()       
+        self.epen=0
+        self.tpen=0
+        self.hpen=0
     
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
-    def step(self, action):
 
-        assert self.action_space.contains(action)
-
-        reward = 0
-        
-        total_t = self.state[0]
-        total_re = self.state[1]
+    def step(self, action): 
+        total_T = self.state[0]
+        total_RE = self.state[1]
         e_price = self.state[2]
         h_price = self.state[3]
         bes = self.state[4]
@@ -123,40 +106,36 @@ class NR_IES_v0(gym.Env):
         t_htse = action[0]        
         t_stg = action[1]  
         e_stg = action[2] 
-        e_pem = action[3] 
-        h_stg = action[4]
-
-        cost = 10000
-
-        #action1  Thermal energy to HTSE
-        total_t -= t_htse
+        e_pem= action[3] 
+        h_stg= action[4] 
         
-        #action2   Thermal energy to thermal energy storage     
-        if tes >= 50000:
-            total_t += t_stg
+        #action1  Thermal power to HTSE
+        total_T -= t_htse
+        
+        #action2   Thermal power to thermal energy storage     
+        if tes>=5000000:
+            total_T += t_stg
         else:
-            total_t -= t_stg
+            total_T -=t_stg
             tes += t_stg
             
-        total_e = total_t + total_re
-
-        #action1 Electric energy to HTSE ??
+        elec = total_T + total_RE
+        #action1 Electric power to HTSE 
         t = t_htse 
-        e = t_htse
+        e = 800*t_htse
         h = (t+e)/33333
         hpr_HTSE += h
-        total_e -= e
-            
-            
+        elec -= e
+
         #action3 Electric power to battery energy storag        
         if bes >= 100000:
-            total_e += e_stg
+            elec += e_stg
         else:
-            total_e -= e_stg
+            elec -=e_stg
             bes += e_stg
         
         #action4  Electric power to PEM 
-        total_e -= e_pem
+        elec -= e_pem
         htr_PEM = e_pem/33333
         
         total_h = hpr_HTSE + htr_PEM
@@ -168,27 +147,29 @@ class NR_IES_v0(gym.Env):
             hss += h_stg
         
         # Reduce episode length by 1 second
-        self.episodlenght +=1 
+        self.episodlenght += 1 
         # Calculate reward
-        profit = ((total_e*e_price)+ (total_h*h_price)) - cost
-        if profit >= 100000:
+        profit = ((elec*e_price) + (total_h*h_price))
+        if profit >= 80000000:
             reward = 1 
         else: 
             reward = -1 
-
         #change reward if storage is deviated 
-        if bes<= 0.2*100000 and bes>= 0.8*100000:
+       
+        if bes <= 0.2*100000:
             reward = -1
-            self.epen += 1                        
-        if tes<= 0.25*5000000 and tes >= 0.90*5000000:
+            self.epen += 1
+
+        if tes <= 0.25*5000000:
             reward = -1
             self.tpen += 1
-        if hss<= 0.3*200 and hss >= 0.90*200:
+
+        if hss <= 0.3*200:
             reward =-1
             self.hpen += 1
 
-        
-         # Check if episode is done
+        # Check if episode is done
+
         if self.episodlenght >= 99: 
             done = True
 
@@ -196,49 +177,46 @@ class NR_IES_v0(gym.Env):
             done = False
             
         observation = self.observation_space.sample()
-        total_t = observation[0]
-        total_re = observation[1]
-
+        total_T =observation[0]
+        total_RE =observation[1]
         e_price = observation[2]
         h_price = observation[3]   
         
         # Set placeholder for info    
         info = {}        
-        # Return step information
-        # statef = []
-        # statef.append(Allvariable(i,f,md))
-        self.state = np.array([total_t, total_re, e_price, h_price, bes, tes, hss, power_grid, hpr_HTSE, htr_PEM, h_distributed])
+
+        self.state = np.array([total_T, total_RE, e_price, h_price, bes, tes, hss, power_grid, hpr_HTSE, htr_PEM, h_distributed])
 
         try:
             assert self.observation_space.contains(self.state)
         except AssertionError:
             print("INVALID STATE", self.state)
-        
+
         return self.state, reward, done, info
     
     def reset(self):
-        # Reset shower temperature
-        # self.state = [600000000,50000000,0.15,2,50000,20000,25,50000,10,5,100]
-        # self.state = [572980350.0, 37890804.0, 0.00015191699, 2.0791678, 16692.ls287, 10548.404, 91.59362, 72073250.0, 364.4513, 85.14204, 483.5188]
-        self.state = [500000010, 10000010, 0.12, 2.0791678, 2100.287, 2054.404, 11.59362, 1100.0, 1.4513, 1.14204, 1.5188]
-        
-        # Reset shower time
-        self.epen = 0
-        self.tpen = 0
-        self.hpen = 0
 
+        # Reset shower temperature
+        self.state = [654711200.0, 70891980.0, 0.11293792, 3.965408, 20702.883, 1507198.8, 42.513252, 244234030.0, 60.791195, 133.09085, 308.13058]
+        # Reset shower time
+        self.epen=0
+        self.tpen=0
+        self.hpen=0
         self.episodlenght = 0
         return self.state
 
-env = NR_IES_v0()
-episodes = 200
-for episode in range(1, episodes+1):
-    state = env.reset()
-    done = False
-    score = 0 
-    while not done:
-        #env.render()
-        action = env.action_space.sample()        
-        n_state, reward, done, info = env.step(action)
-        score+=reward
-    print('Episode:{} Score:{}'.format(episode, score))
+
+# env = CostomIESEnv()
+# episodes = 2
+# for episode in range(1, episodes+1):
+#     state = env.reset()
+#     done = False
+#     score = 0 
+
+#     while not done:
+#         #env.render()
+#         action = env.action_space.sample()        
+#         n_state, reward, done, info = env.step(action)
+
+#         score+=reward
+#     print('Episode:{} Score:{}'.format(episode, score))
